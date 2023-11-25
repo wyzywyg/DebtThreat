0473ea7eb384e113cad88f4cfc5fafbd29bb8832
par-paulreyes <124680300+par-paulreyes@users.noreply.github.com>
2023-11-25 12:51:21 +0800

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,29 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+class ScenarioForm(FlaskForm):
+    choice = SelectField('Choose Decision', choices=[], coerce=str)
+    def __init__(self, choices=None, **kwargs):
+        super(ScenarioForm, self).__init__(**kwargs)
+        if choices:
+            self.choice.choices = choices
+            
 class NewGameForm(FlaskForm):
-    name = StringField('', validators=[DataRequired()])
+    name = StringField(validators=[DataRequired()])
     submit = SubmitField('Start New Game')
 
 class DifficultyForm(FlaskForm):
-    difficulty = SelectField('Choose Difficulty', choices=[('easy', '96-100'), ('normal', '90-95'), ('hard', '80-89'), ('hardcore', 'Below 79')])
+    difficulty = SelectField('Choose Difficulty', choices=[('easy', 'Easy'), ('normal', 'Normal'), ('hard', 'Hard'), ('hardcore', 'Hardcore')])
 
 class UniversityForm(FlaskForm):
     university_type = SelectField('Choose University Type', choices=[('public', 'Public'), ('private', 'Private')])
-    submit = SubmitField('Continue')
 
 class ProgramForm(FlaskForm):
     program = SelectField('Choose Program', choices=[('civil_engineering', 'Civil Engineering'), ('nursing', 'Nursing'), ('computer_science', 'Computer Science'), ('fine_arts', 'Fine Arts')])
-    submit = SubmitField('Continue')
 
-class DormForm(FlaskForm):
+class DormForm(FlaskForm): 
     dorm_type = SelectField('Choose Dorm Type', choices=[('solo_room', 'Solo Room'), ('bed_space', 'Bed Space')])
-    submit = SubmitField('Continue')
+    
 
-class FirstScenarioForm(FlaskForm):
-    choice = SelectField('Choose Decision', choices=[('a', ' '), ('b', ' ')])
-    submit = SubmitField('Submit')
