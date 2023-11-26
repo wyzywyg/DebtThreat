d25e3de4a84caa61e51ff65ed134d3167e6d6a74
JaronLouise <123366766+JaronLouise@users.noreply.github.com>
2023-11-26 16:30:01 +0800

    Update scenarios.py

--- scenarios.py
+++ scenarios.py
@@ -24,6 +24,18 @@ SCENARIO_DATA = {
 
     'BD': {'options': [('A', 'Take summer internship'), ('B', 'Take summer vacation')],
            'outcomes': {'A': (7000, 7, 5, 5), 'B': (-5000, -7, 5, 5)}},
+    
+    'CA': {'options': [('A', 'Use loan'), ('B', 'Prioritize studies')],
+           'outcomes': {'A': (-15000, 10, 5, 15), 'B': (0, 5, -7, -10)}},
+
+    'CB': {'options': [('A', 'Accept'), ('B', 'Decline')],
+           'outcomes': {'A': (-85000, 15, 0, 5), 'B': (0, 5, 0, 5)}},
+
+    'CC': {'options': [('A', 'Dedicate time on relationship'), ('B', 'Prioritize studies')],
+           'outcomes': {'A': (-10000, -10, 5, 5), 'B': (0, 10, -5, -5)}},
+
+    'CD': {'options': [('A', 'Invest savings'), ('B', 'Research and plan')],
+           'outcomes': {'A': (-30000, 15, -5, 5), 'B': (-5000, 5, -3, 3)}},
 
     # Add more scenarios as needed...
 }
